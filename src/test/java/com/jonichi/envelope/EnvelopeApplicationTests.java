97d0bbb68a0ab41fa2bcc49b44c3cfa1017fd1af
jonichi <jonichi16@gmail.com>
2024-09-30 18:07:24 +0800

    initial commit

---
+++ src/test/java/com/jonichi/envelope/EnvelopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonichi.envelope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvelopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

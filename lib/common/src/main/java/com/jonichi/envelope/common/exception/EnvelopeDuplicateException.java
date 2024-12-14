21c0f314ecd1a8ffc3b7513f7a83b30b6ac6a69c
jonichi <jonichi16@gmail.com>
2024-12-14 15:57:38 +0800

    feat: add EnvelopeDuplicateException and handler

---
+++ lib/common/src/main/java/com/jonichi/envelope/common/exception/EnvelopeDuplicateException.java
@@ -0,0 +1,7 @@
+package com.jonichi.envelope.common.exception;
+
+public class EnvelopeDuplicateException extends RuntimeException {
+    public EnvelopeDuplicateException(String message) {
+        super(message);
+    }
+}

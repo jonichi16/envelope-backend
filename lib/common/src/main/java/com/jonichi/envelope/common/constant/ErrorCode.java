95982cf0d1c4fc587435792f6c073f35aaee3291
jonichi <jonichi16@gmail.com>
2024-12-14 15:31:32 +0800

    feat: add exception handlers

---
+++ lib/common/src/main/java/com/jonichi/envelope/common/constant/ErrorCode.java
@@ -0,0 +1,10 @@
+package com.jonichi.envelope.common.constant;
+
+public class ErrorCode {
+    public static final String NESTED_ERROR = "ERR_001";
+    public static final String NOT_FOUND = "ERR_002";
+    public static final String DUPLICATE = "ERR_003";
+    public static final String MISSING_REQUIRED = "ERR_004";
+    public static final String UNAUTHORIZED = "ERR_005";
+    public static final String INTERNAL_SERVER_ERROR = "ERR_500";
+}

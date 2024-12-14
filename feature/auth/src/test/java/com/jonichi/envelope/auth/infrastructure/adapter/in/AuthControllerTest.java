c3296f238de8f92989280b63c1311eabb620f88d
jonichi <jonichi16@gmail.com>
2024-12-14 10:42:32 +0800

    feat: add register controller happy path

--- feature/auth/src/test/java/com/jonichi/envelope/auth/infrastructure/adapter/in/AuthControllerTest.java
+++ feature/auth/src/test/java/com/jonichi/envelope/auth/infrastructure/adapter/in/AuthControllerTest.java
@@ -1,15 +1,17 @@
 package com.jonichi.envelope.auth.infrastructure.adapter.in;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jonichi.envelope.auth.application.port.in.AuthUseCase;
+import com.jonichi.envelope.auth.infrastructure.adapter.in.dto.AuthTokenDTO;
 import com.jonichi.envelope.auth.infrastructure.adapter.in.dto.RegisterRequestDTO;
+import com.jonichi.envelope.common.dto.ApiResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.util.Objects;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.times;
@@ -45,39 +47,24 @@ public class AuthControllerTest {
         );
     }
 
-//    @Test
-//    public void register_shouldReturnCorrectResponseBody() throws Exception {
-//        // given
-//        String username = "test";
-//        String email = "test@mail.com";
-//        String password = "secret";
-//
-//        RegisterRequestDTO registerRequestDTO = new RegisterRequestDTO(username, email, password);
-//
-//        // when
-//        when(authUseCase.register(username, email, password)).thenReturn("encodedToken");
-//        ResponseEntity<?> response = authController.register(registerRequestDTO);
-//
-//        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-//        assertThat(response.getBody()).isInstanceOf(SuccessResponse.class);
-//
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        String responseBodyJson = objectMapper.writeValueAsString(response.getBody());
-//        SuccessResponse1<String> actualResponse = objectMapper
-//                .readValue(
-//                        responseBodyJson,
-//                        objectMapper
-//                                .getTypeFactory()
-//                                .constructParametricType(
-//                                        SuccessResponse1.class,
-//                                        String.class
-//                                )
-//                );
-//
-//        // Validate fields
-//        assertThat(actualResponse.getCode()).isEqualTo(201);
-//        assertThat(actualResponse.getMessage()).isEqualTo("User registered successfully");
-//        assertThat(actualResponse.getData()).isEqualTo("encodedToken");
-//    }
+    @Test
+    public void register_shouldReturnCorrectResponseBody() throws Exception {
+        // given
+        String username = "test";
+        String email = "test@mail.com";
+        String password = "secret";
+
+        RegisterRequestDTO registerRequestDTO = new RegisterRequestDTO(username, email, password);
+
+        // when
+        when(authUseCase.register(username, email, password)).thenReturn("encodedToken");
+        ResponseEntity<ApiResponse<AuthTokenDTO>> response = authController.register(registerRequestDTO);
+
+        assertThat(response.getStatusCode().value()).isEqualTo(201);
+        assertThat(Objects.requireNonNull(response.getBody()).getData().token()).isEqualTo("encodedToken");
+        assertThat(response.getBody().getMessage()).isEqualTo("User registered successfully");
+        assertThat(response.getBody().getCode()).isEqualTo(201);
+
+    }
 
 }

27f08a7442ce1756517d03f5b3a4f312d0e5e39f
jonichi <jonichi16@gmail.com>
2024-12-15 08:48:12 +0800

    feat: add jpa, liquibase, postgresql dependencies

--- feature/auth/src/test/java/com/jonichi/envelope/auth/infrastructure/adapter/in/IntegrationTest.java
+++ feature/auth/src/test/java/com/jonichi/envelope/auth/infrastructure/adapter/in/IntegrationTest.java
@@ -18,7 +18,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
-@Import(GlobalExceptionHandler.class)
 public class IntegrationTest {
 
     @Autowired

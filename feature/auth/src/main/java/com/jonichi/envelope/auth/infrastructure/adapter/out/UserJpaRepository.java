40183bb3ebc5ffc0fe7dad8a81dd8b945b26282b
jonichi <jonichi16@gmail.com>
2024-12-15 12:45:47 +0800

    feat: add findByUsername in UserJpaRepository

--- feature/auth/src/main/java/com/jonichi/envelope/auth/infrastructure/adapter/out/UserJpaRepository.java
+++ feature/auth/src/main/java/com/jonichi/envelope/auth/infrastructure/adapter/out/UserJpaRepository.java
@@ -1,9 +1,11 @@
 package com.jonichi.envelope.auth.infrastructure.adapter.out;
 
 import com.jonichi.envelope.auth.infrastructure.adapter.out.model.UserEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserJpaRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByUsername(String username);
 }
--- feature/auth/src/main/java/com/jonichi/envelope/auth/infrastructure/adapter/out/UserRepository.java
+++ feature/auth/src/main/java/com/jonichi/envelope/auth/infrastructure/adapter/out/UserRepository.java
@@ -1,20 +1,30 @@
 package com.jonichi.envelope.auth.infrastructure.adapter.out;
 
+import com.jonichi.envelope.auth.application.port.out.UserRepositoryPort;
 import com.jonichi.envelope.auth.domain.User;
 import com.jonichi.envelope.auth.infrastructure.adapter.out.mapper.UserMapper;
 import com.jonichi.envelope.common.util.listener.TransactionalHandler;
+import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepository {
+public class UserRepository implements UserRepositoryPort {
 
     private UserJpaRepository userJpaRepository;
     private TransactionalHandler transactionalHandler;
 
+    @Override
     public void save(User user) {
         transactionalHandler.runInTransaction(() ->
                 userJpaRepository.save(UserMapper.toUserEntity(user))
         );
 
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return userJpaRepository.findByUsername(username)
+                .map(UserMapper::toUser)
+                .or(Optional::empty);
     }
 }

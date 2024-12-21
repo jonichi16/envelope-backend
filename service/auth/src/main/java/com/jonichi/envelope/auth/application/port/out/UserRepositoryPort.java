29299ea6e957f48a7db0a240597f590ecce5f3d9
jonichi <jonichi16@gmail.com>
2024-12-21 19:29:45 +0800

    docs: update java docs for auth token

--- service/auth/src/main/java/com/jonichi/envelope/auth/application/port/out/UserRepositoryPort.java
+++ service/auth/src/main/java/com/jonichi/envelope/auth/application/port/out/UserRepositoryPort.java
@@ -4,25 +4,34 @@ import com.jonichi.envelope.auth.domain.User;
 import java.util.Optional;
 
 /**
- * A port for managing user persistence operations in the application.
+ * Port interface for user repository operations.
  *
- * <p>The {@code UserRepositoryPort} defines the contract for the operations
- * required to manage user entities in the persistence layer.</p>
+ * <p>This interface defines the contract for interacting with the user repository.
+ * Implementations of this interface should handle data persistence and retrieval
+ * related to user entities.</p>
  */
 public interface UserRepositoryPort {
 
     /**
-     * Saves a {@link User} entity to the persistence layer.
+     * Saves the given user entity to the repository.
+     *
+     * <p>If the user entity already exists, this method updates the existing record.
+     * Otherwise, it creates a new record in the repository.</p>
      *
      * @param user the {@link User} entity to save
+     * @return the saved {@link User} entity
      */
     User save(User user);
 
     /**
-     * Finds a {@link User} entity by its username.
+     * Finds a user entity by their username.
      *
-     * @param username the username of the {@link User} to find
-     * @return an {@link Optional} containing the {@link User} if found, or empty if not found
+     * <p>This method searches the repository for a user entity with the specified username.
+     * If a matching user is found, it is returned as an {@link Optional}; otherwise, an empty
+     * {@link Optional} is returned.</p>
+     *
+     * @param username the username of the user to find
+     * @return an {@link Optional} containing the {@link User} if found, or empty if not
      */
     Optional<User> findByUsername(String username);
 }
